2436020db9b5e0cf0f8aa1a4ed9de7ec8662958c
visum-summerschool <master@eusebio.c.visum2018-209013.internal>
2019-07-04 11:06:04 +0000

    code refactoring

--- VISUM_baseline/VISUM_FasterRCNN.py
+++ VISUM_baseline/VISUM_FasterRCNN.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 import torch.utils.data
 import torchvision
@@ -9,82 +10,86 @@ from utils_ import transforms as T
 from utils_.visum_utils import VisumData
 
 
+def main():
+    parser = argparse.ArgumentParser(description='VISUM 2019 competition - baseline training script', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-d', '--data_path', default='/home/master/dataset/train', metavar='', help='data directory path')
+    parser.add_argument('-m', '--model_path', default='./baseline.pth', metavar='', help='model file (output of training)')
+    parser.add_argument('--epochs', default=50, type=int, metavar='', help='number of epochs')
+    parser.add_argument('--lr', default=0.005, type=float, metavar='', help='learning rate')
+    parser.add_argument('--l2', default=0.0005, type=float, metavar='', help='L-2 regularization')
+    args = vars(parser.parse_args())
 
-DATA_DIR = '/home/master/dataset/train'
-SAVE_MODEL = './'
+    # Data augmentation
+    def get_transform(train):
+        transforms = []
+        # converts the image, a PIL image, into a PyTorch Tensor
+        transforms.append(T.ToTensor())
+        if train:
+            # during training, randomly flip the training images
+            # and ground-truth for data augmentation
+            transforms.append(T.RandomHorizontalFlip(0.5))
+        return T.Compose(transforms)
+
+    backbone = torchvision.models.mobilenet_v2(pretrained=True).features
+    backbone.out_channels = 1280
+
+    anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
+                                    aspect_ratios=((0.5, 1.0, 2.0),))
+
+    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=[0],
+                                                    output_size=7,
+                                                    sampling_ratio=2)
 
 
-# Data augmentation
-def get_transform(train):
-    transforms = []
-    # converts the image, a PIL image, into a PyTorch Tensor
-    transforms.append(T.ToTensor())
-    if train:
-        # during training, randomly flip the training images
-        # and ground-truth for data augmentation
-        transforms.append(T.RandomHorizontalFlip(0.5))
-    return T.Compose(transforms)
+    # put the pieces together inside a FasterRCNN model
+    model = FasterRCNN(backbone,
+                    num_classes=10,
+                    rpn_anchor_generator=anchor_generator,
+                    box_roi_pool=roi_pooler)
 
-backbone = torchvision.models.mobilenet_v2(pretrained=True).features
+    # See the model architecture
+    print(model)
 
-backbone.out_channels = 1280
+    # use our dataset and defined transformations
+    dataset = VisumData(args['data_path'], modality='rgb', transforms=get_transform(train=True))
+    dataset_val = VisumData(args['data_path'], modality='rgb', transforms=get_transform(train=False))
 
-anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
-                                   aspect_ratios=((0.5, 1.0, 2.0),))
+    # split the dataset in train and test set
+    torch.manual_seed(1)
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-100])
+    dataset_val = torch.utils.data.Subset(dataset_val, indices[-100:])
 
-roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=[0],
-                                                output_size=7,
-                                                sampling_ratio=2)
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
 
+    data_loader_val = torch.utils.data.DataLoader(
+        dataset_val, batch_size=2, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
 
-# put the pieces together inside a FasterRCNN model
-model = FasterRCNN(backbone,
-                   num_classes=10,
-                   rpn_anchor_generator=anchor_generator,
-                   box_roi_pool=roi_pooler)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-# See the model architecture
-print(model)
+    model.to(device)
 
-# use our dataset and defined transformations
-dataset = VisumData(DATA_DIR, modality='rgb', transforms=get_transform(train=True))
-dataset_val = VisumData(DATA_DIR, modality='rgb', transforms=get_transform(train=False))
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=args['lr'],
+                                momentum=0.9, weight_decay=args['l2'])
 
-# split the dataset in train and test set
-torch.manual_seed(1)
-indices = torch.randperm(len(dataset)).tolist()
-dataset = torch.utils.data.Subset(dataset, indices[:-100])
-dataset_val = torch.utils.data.Subset(dataset_val, indices[-100:])
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=10,
+                                                   gamma=0.5)
 
-# define training and validation data loaders
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=2, shuffle=True, num_workers=4,
-    collate_fn=utils.collate_fn)
+    for epoch in range(args['epochs']):
+        # train for one epoch, printing every 10 iterations
+        epoch_loss = train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluator = evaluate(model, data_loader_val, device=device)
 
-data_loader_val = torch.utils.data.DataLoader(
-    dataset_val, batch_size=2, shuffle=False, num_workers=4,
-    collate_fn=utils.collate_fn)
+    torch.save(model, args['model_path'])
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-model.to(device)
-
-params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005,
-                            momentum=0.9, weight_decay=0.0005)
-
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                               step_size=10,
-                                               gamma=0.5)
-
-num_epochs = 50
-
-for epoch in range(num_epochs):
-    # train for one epoch, printing every 10 iterations
-    epoch_loss = train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-    # update the learning rate
-    lr_scheduler.step()
-    # evaluate on the test dataset
-    evaluator = evaluate(model, data_loader_val, device=device)
-
-torch.save(model, SAVE_MODEL)
+if __name__ == '__main__':
+    main()
